4f529e21a47cdbcab37916be6f4017172e9fa628
Mariam Emad <93050116+Mariam-Emad127@users.noreply.github.com>
2025-04-03 02:48:07 +0200

    Update index.js

--- Chat-Server-master/index.js
+++ Chat-Server-master/index.js
@@ -15,17 +15,45 @@ io.on("connection", (socket) => {
   console.log(socket.id, "has joined");
   socket.on("signin", (id) => {
     console.log(id);
-    clients[id] = socket;
+    clients[id] = socket.id;
     console.log(clients);
+    console.log(`User signed in: ${id} => ${socket.id}`);
+    console.log("All connected clients:", clients); // ✅ هتطبع كل المستخدمين المتصلين
   });
+  /*
   socket.on("message", (msg) => {
-    console.log(msg);
-    //socket.broadcast.emit("message", {msg,type:"othermessage"})
-  
+    console.log(msg);  
        socket.emit("message",{...msg,type:"othermessage"} )
-    //let reciver = msg.reciver;
-    //if (clients[reciver]) clients[reciver].emit("message", msg);
-    //console.log(msg);
+    let reciver = msg.reciver;
+   // if (clients[reciver]) clients[reciver].emit("message", {...msg,type:"othermessage"});
+    console.log(reciver);
+  });
+});
+*/
+socket.on("message", (msg) => {
+  console.log("Received message:", msg);
+
+  let reciver = msg.reciver;
+  let reciverSocketId = clients[reciver]; // Socket ID للطرف الآخر
+  if (reciverSocketId) {
+    // ✅ إرسال الرسالة فقط للطرف المستهدف
+    io.to(reciverSocketId).emit("message", {
+      ...msg,
+      type: "othermessage",
+    });
+    console.log(`Message sent to: ${reciver}`);
+  } else {
+    console.log(`User ${reciver} is offline`);
+  }
+});
+ 
+  // ✅ حذف المستخدم من القائمة عند قطع الاتصال
+  socket.on("disconnect", () => {
+    let userId = Object.keys(clients).find((key) => clients[key] === socket.id);
+    if (userId) {
+      delete clients[userId];
+      console.log(`User ${userId} disconnected`);
+    }
   });
 });
 
